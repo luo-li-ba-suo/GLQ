64cf5d6cfc8f4a6d680be34080ca6234813e9144
dq <luo_li_ba_suo@163.com>
2023-02-21 17:04:49 +0800

    将工程所需功能包附在setup.py中

--- offpolicy/__init__.py
+++ offpolicy/__init__.py
@@ -1,7 +1,7 @@
 from offpolicy import algorithms, envs, runner, scripts, utils, config
 
 
-__version__ = "0.1.0"
+__version__ = "1.1.0"
 
 __all__ = [
     "algorithms",
--- setup.py
+++ setup.py
@@ -4,17 +4,20 @@
 import os
 from setuptools import setup, find_packages
 
+
 def get_version() -> str:
     # https://packaging.python.org/guides/single-sourcing-package-version/
     init = open(os.path.join("offpolicy", "__init__.py"), "r").read().split()
     return init[init.index("__version__") + 2][1:-1]
 
+
 setup(
     name="offpolicy",  # Replace with your own username
     version=get_version(),
-    description="off-policy algorithms of marlbenchmark",
+    description="GLQ and off-policy algorithms of marlbenchmark",
     long_description=open("README.md", encoding="utf8").read(),
     long_description_content_type="text/markdown",
+    author='kideng',
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -27,4 +30,11 @@ setup(
     ],
     keywords="multi-agent reinforcement learning platform pytorch",
     python_requires='>=3.6',
+    install_requires=[
+        "wandb",
+        "torch",
+        "gym==0.11",
+        "seaborn",
+        "tensorboardX"
+    ]
 )
